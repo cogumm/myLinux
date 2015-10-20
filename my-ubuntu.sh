a9aaefc211c48836423710fa849045cd18700f20
Gabriel F. Vilar <cogumm@gmail.com>
2015-10-19 23:37:13 -0200

    Updade main shell

--- my-ubuntu.sh
+++ my-ubuntu.sh
@@ -11,6 +11,7 @@
 #   29-04-13 => CoGUMm  - Atualizado alguns pacotes, adicionado a instalação do Ruby, RoR e Sublime2
 #   14-10-15 => CoGUMm  - Atualizado vários pacores, adicionado a instalação Telegram, Wine e Popcorn Time
 #   16-10-15 => CoGUMm  - Adicionado Spotify
+#   19-10-15 => CoGUMm  - Criado um novo arquivo só para programas de desenvolvimento.
 #---------------------------------------------------------------
 
 # script variables
@@ -25,70 +26,6 @@ installServerUpdates()
     sudo apt-get update
     sudo apt-get upgrade
     sudo apt-get dist-upgrade
-
-    return
-}
-
-# VIM
-installVim()
-{
-    sudo apt-get install -y vim
-
-    return
-}
-# Instalando Apache
-installApache()
-{
-    sudo apt-get install -y apache2
-
-    return
-}
-
-# Instalando MySQL
-installMySQL()
-{
-    sudo apt-get install -y mysql-server-5.6
-
-    echo ""
-    echo "Type your MySQL root password:"
-    echo "\
-        CREATE DATABASE \`${username}\` CHARACTER SET utf8; \
-        USE \`${username}_production\`; \
-        GRANT ALL PRIVILEGES ON \`${username}\`.* TO '$username'@'localhost' IDENTIFIED BY '$mysql_password'; \
-        FLUSH PRIVILEGES;" | mysql -u root -p
-
-    sudo /etc/init.d/mysql restart
-
-    cd /var/
-    sudo chmod -R 777 www/
-    cd www/
-    echo "<?php phpinfo() ?>"  >> info.php
-
-    return
-}
-
-# Instalando PHP5
-installPHP()
-{
-    sudo apt-get install -y php5 php5-cli php5-curl php5-mysql php5-pgsql php5-sqlite
-
-    return
-}
-
-# Instalando phpMyAdmin
-installphpMyAdmin()
-{
-    sudo apt-get install -y phpmyadmin
-
-    sudo /etc/init.d/apache2 restart
-
-    return
-}
-
-# Instalando Git
-installGit()
-{
-    sudo apt-get install -y git-core git-gui git-doc gitg
 
     return
 }
@@ -211,17 +148,6 @@ OpenOfficeLibreOffice()
     return
 }
 
-
-# GEdit
-installGedit()
-{
-    sudo apt-add-repository ppa:ubuntu-on-rails/ppa
-    sudo apt-get update
-    sudo apt-get install -y gedit-gmate
-
-    return
-}
-
 # Ubuntu-Tweak
 installUbuntuTweak()
 {
@@ -232,49 +158,12 @@ installUbuntuTweak()
     return
 }
 
-# PostgreSQL
-installPG()
-{
-    sudo apt-get install -y postgresql
-    sudo apt-get install -y pgadmin3
-
-    sudo su postgres -c psql postgres
-
-    echo ""
-    echo "Type your PG root password:"
-    echo "\
-        ALTER USER postgres WITH PASSWORD 'root'; \
-        \q;"
-
-    sudo /etc/init.d/postgresql restart
-
-    return
-}
-
 # VLC
 installVLC()
 {
     sudo add-apt-repository ppa:ferramroberto/vlc
     sudo apt-get update
     sudo apt-get install -y vlc mozilla-plugin-vlc
-
-    return
-}
-
-# MySQL-Workbench
-installMySQLWorkbench()
-{
-    cd ~
-    if [ -e temp ]
-    then
-        echo "Diretório já existe."
-        cd ~/temp/
-    else
-        mkdir temp
-        cd ~/temp/
-    fi
-    wget http://dl.dropbox.com/u/1774817/myUbuntu/mysql-workbench-gpl-5.2.33b-1ubu1010-i386.deb
-    sudo dpkg -i mysql-workbench-gpl-5.2.33b-1ubu1010-i386.deb
 
     return
 }
@@ -297,26 +186,6 @@ installTeamViewer()
     return
 }
 
-
-# Eclipse
-installEclipse()
-{
-    cd ~
-    if [ -e temp ]
-    then
-        echo "Diretório já existe."
-        cd ~/temp/
-    else
-        mkdir temp
-        cd ~/temp/
-    fi
-    wget http://dl.dropbox.com/u/1774817/myUbuntu/eclipse-php-helios-SR2-linux-gtk.tar.gz
-    tar -zxvf eclipse-php-helios-SR2-linux-gtk.tar.gz
-    sudo mv eclipse /opt/
-
-    return
-}
-
 # Skype
 installSkype()
 {
@@ -335,7 +204,6 @@ installSkype()
     return
 }
 
-
 # Gestão de arquivo
 installZIP()
 {
@@ -353,35 +221,6 @@ installAll()
     sudo apt-get install -y gtranslator
     sudo apt-get install -y xchat
     sudo apt-get install -y filezilla
-
-    return
-}
-
-# RVM
-installRuby()
-{
-     curl -L https://get.rvm.io | bash -s stable --rails --autolibs=enabled
-     rvm install 1.9.3
-     rvm use --default 1.9.3
-
-     return
-}
-
-# RoR
-installRoR()
-{
-    gem install rails
-    gem install mysql2
-
-    return
-}
-
-# Sublime Text 2
-installSublime()
-{
-    sudo add-apt-repository ppa:webupd8team/sublime-text-2
-    sudo apt-get update
-    sudo apt-get install sublime-text-2-dev
 
     return
 }
@@ -419,24 +258,6 @@ installPopCorn()
     fi
     wget -O - "https://get.popcorntime.io/build/Popcorn-Time-0.3.8-5-Linux-64.tar.xz" | tar xzf -
     sudo ./install
-
-    return
-}
-
-# Atom
-installAtom()()
-{
-    cd ~
-    if [ -e temp ]
-    then
-        echo "Diretório já existe."
-        cd ~/temp/
-    else
-        mkdir temp
-        cd ~/temp/
-    fi
-    wget https://github-cloud.s3.amazonaws.com/releases/3228505/873ec70a-6378-11e5-96e0-a5349100d0aa.deb?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAIMWPLRQEC4XCWWPA%2F20151015%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20151015T031937Z&X-Amz-Expires=3600&X-Amz-Signature=c4e01c7e6973f8868453388dadb8613cefdb07c0136ca74623f038693f444961&X-Amz-SignedHeaders=host&response-content-disposition=attachment%3B%20filename%3Datom-amd64.deb&response-content-type=application%2Foctet-stream
-    sudo dpkg -i atom-amd64.deb
 
     return
 }
@@ -507,58 +328,30 @@ exitIfCancelOrESC $input
 username=`cat $tmpFile`
 clear
 
-# MySQL senha
-#---------------------------------------------------------------
-dialog  --backtitle "$mainTitle" --title "Conta de usuário do MySQL" \
-        --inputbox "Um usuário será adicionado ao MySQL '$username'. Além disso, um banco de dados chamado '${username}' também será criado.\n\nPor favor, digite uma nova senha. para este usuário MySQL." \
-        13 50 2> $tmpFile
-
-input=$?
-exitIfCancelOrESC $input
-mysql_password=`cat $tmpFile`
-clear
-
 # Start the process
 #---------------------------------------------------------------
 installServerUpdates
-installVim
-installApache
-installMySQL
-installPHP
-installphpMyAdmin
-installGit
 installDocky
 installDropbox
 #installGoogleChrome
 installGoogleTalkPlugin
 #installjDownload
 #OpenOfficeLibreOffice
-installGedit
 #installUbuntuTweak
-#installPG
 installVLC
-#installMySQLWorkbench
 #installTeamViewer
-#installEclipse
 #installSkype
 installZIP
 installAll
-#installRuby
-#installRoR
-installSublime
 installTelegram
 installWine
 installPopCorn
-installAtom
 installSpotify
 installClean
 
 exitScript
 exitIfCancelOrESC
 
-
-dialog  --backtitle "$mainTitle" --title "Instalação completa!" \
-        --msgbox "Para testar o Apache+PHP acesse http://$domain/info.php e veja se esta tudo ok." 8 50
 
 clear
 exit 0
