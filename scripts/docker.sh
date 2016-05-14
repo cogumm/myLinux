8a3331c4cdc486f97d6a7bd2107312d27a57a716
Gabriel F. Vilar <cogumm@gmail.com>
2016-05-14 19:24:38 -0300

    Adicionado script para Docker

---
+++ scripts/docker.sh
@@ -0,0 +1,94 @@
+#!/bin/bash
+
+echo "---------------------------------------------------------------"
+echo "Desenvolvido por Gabriel F. Vilar"
+echo "E-mail: cogumm@gmail.com"
+echo "Website: http://cogumm.net"
+echo "NÃO ME RESPONSABILIZO POR MAUS DANOS À MÁQUINA COM ESTE SCRIPT,\nFAVOR UTILIZÁ-LO SE APENAS TIVER CERTEZA DO QUE ESTEJA FAZENDO."
+echo "---------------------------------------------------------------"
+
+# Verificando e validando se é root
+if [ `id -u` -ne 0 ]; then
+	echo
+		echo "Você precisa ter poderes administrativos (root)"
+		echo "O script está sendo finalizado.."
+		sleep 4
+		exit
+fi
+
+
+installDocker()
+{
+    if [ which -a docker ]
+    then
+        echo "Docker já instalado"
+    else
+        echo "Instalando e configurando o Docker, aguarde.."
+        curl -sSl https://get.docker.com | sh
+        sudo usermod -aG docker ${USER}  
+        sudo service docker restart
+    fi
+    
+    return
+}
+
+checkDocker()
+{
+    docker images && docker ps -a
+}
+
+cleanDocker()
+{
+    docker rm -f $(docker ps -qa) && docker rmi $(docker images -f "dangling=true" -q)
+
+}
+
+runmyDocker()
+{
+    echo "Rodando apache-php"
+    docker run -v /home/cogumm/dev/www:/app -d -p 80:80 tutum/apache-php
+    echo
+    echo "Entrando no container"
+    docker exec -it 145c0ea49a2d bash
+}
+
+echo "Bem vindo ao script de gerenciamento do Docker."
+echo
+echo "1) Instalar Docker "
+echo
+echo "2) Ver todas as imagens e containers no host "
+echo
+echo "3) Limpar Docker "
+echo
+echo "4) Rodar container "
+echo
+echo "s) Sair "
+
+read -p "Para continuar escolha uma das opções  " escolha
+	case $escolha in
+		1) echo
+	        installDocker
+	        sleep 2
+	        ;;
+		2) echo
+	        checkDocker
+	        sleep 2
+	        ;;
+		3) echo
+	        cleanDocker
+	        sleep 2
+	        ;;
+		4) echo
+	        runmyDocker
+	        sleep 2
+	        ;;
+		s|S) echo
+		    echo "Saindo.."
+	        exitScript
+	        ;;
+		*) echo
+		    echo Alternativa incorreta!! Saindo..
+			sleep 1
+			exit
+			;;
+	esac
