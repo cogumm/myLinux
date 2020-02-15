f7e633f74b5ff729c953262e65c985e3ef7b6c4d
Gabriel F. Vilar <cogumm@gmail.com>
2020-02-15 20:54:05 -0300

    Atualizado para verificar as respostas.

--- carcara/app/scripts/system/updateDistro.sh
+++ carcara/app/scripts/system/updateDistro.sh
@@ -14,35 +14,49 @@ clear
 updateDistro()
 {
 	# update
-    sudo apt-get update -y | \
-	yad --progress \
-	     --title "Atualizando o sistema" \
-	     --text "Re sincronizando os ficheiros de índices dos pacotes a partir das suas fontes." \
-	     --progress-text="Atualizando o sistema." \
-	     --pulsate $configYad
+	echo -n "Atualizar o sistema [update] (y/n)? "
+	read answer
+	if [ "$answer" != "${answer#[Yy]}" ] ;then
+		sudo apt-get update -y | \
+		yad --progress \
+			--title "Atualizando o sistema" \
+			--text "Re sincronizando os ficheiros de índices dos pacotes a partir das suas fontes." \
+			--progress-text="Atualizando o sistema." \
+			--pulsate $configYad
+	fi
 
 	# dist-upgrade
-	sudo apt-get dist-upgrade -y | \
-	yad --progress \
-	     --title "Atualizando o sistema" \
-	     --text "Lidando inteligentemente com as alterações de dependências com as novas versões de pacotes" \
-	     --progress-text="Atualizando o sistema." \
-	     --pulsate $configYad
+	echo -n "Atualizar o sistema [dist-upgrade] (y/n)? "
+	read answer
+	if [ "$answer" != "${answer#[Yy]}" ] ;then
+		sudo apt-get dist-upgrade -y | \
+		yad --progress \
+			--title "Atualizando o sistema" \
+			--text "Lidando inteligentemente com as alterações de dependências com as novas versões de pacotes" \
+			--progress-text="Atualizando o sistema." \
+			--pulsate $configYad
 
-	yad --text="Sistema atualizado." $configYad
+		yad --text="Sistema atualizado." $configYad
+	fi
 
 	# upgrade
-	sudo apt-get upgrade -y | \
-	yad --progress \
-	     --title "Atualizando os pacotes" \
-	     --text "Instando as versões mais recentes de todos os pacotes presentemente instalados no sistema." \
-	     --progress-text="Atualizando o sistema." \
-	     --pulsate $configYad
+	echo -n "Atualizar o sistema [upgrade] (y/n)? "
+	read answer
+	if [ "$answer" != "${answer#[Yy]}" ] ;then
 
-	yad --text="Pacotes atualizado." $configYad
+		sudo apt-get upgrade -y | \
+		yad --progress \
+			--title "Atualizando os pacotes" \
+			--text "Instando as versões mais recentes de todos os pacotes presentemente instalados no sistema." \
+			--progress-text="Atualizando o sistema." \
+			--pulsate $configYad
+
+		yad --text="Pacotes atualizado." $configYad
+	fi
 
 	# Finalizando a atualização
-	yad --title "Atualizando o sistema" --text "Sistema atualizado com sucesso!" $configYad --timeout 3
+	yad --title "Atualizando o sistema"
+		--text "Sistema atualizado com sucesso!" $configYad --timeout 5
 }
 
 updateDistro
